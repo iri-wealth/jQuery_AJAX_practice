bd4714c78694aa892a8405d24ab499b23d7f8a50
iri-wealth <irina_kainz@yahoo.com>
2023-05-02 10:07:39 +0200

    end of application

--- jQuery_AJAX/DOM_manipulation/bankist_advanced.js
+++ jQuery_AJAX/DOM_manipulation/bankist_advanced.js
@@ -293,6 +293,12 @@ slider();
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
+//document.addEventListener("DOMContentLoaded", function (e) {
+//console.log("HTML parsed and DOM loaded", e);
+//});
+//window.addEventListener("load", function (e) {
+//console.log("Page fully loaded", e);
+//});
 
 /*
 ///////////////////////////////////////
