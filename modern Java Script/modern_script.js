1482a9bd6450837eda092d292fec6a199fb25465
iri-wealth <irina_kainz@yahoo.com>
2023-05-03 16:11:21 +0200

    added npm packages

--- modern Java Script/modern_script.js
+++ modern Java Script/modern_script.js
@@ -16,7 +16,7 @@ console.log("Importing module");
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
 
-import add, { cart } from "./shoppingCart.js";
+import add, { cart } from "shoppingCart.js";
 add("pizza", 2);
 add("bread", 5);
 add("apples", 4);
