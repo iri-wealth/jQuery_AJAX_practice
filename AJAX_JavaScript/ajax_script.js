059a6e4f32ea1f361554717116dc5d97b1da9fde
iri-wealth <irina_kainz@yahoo.com>
2023-05-03 09:51:45 +0200

    added ajax html css

--- AJAX_JavaScript/ajax_script.js
+++ AJAX_JavaScript/ajax_script.js
@@ -4,10 +4,19 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all");
+/*const request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "https://api.countrylayer.com/v2/all?access_key=241f2d3a72f886e0b39c522b98ea1650"
+);
 request.send();
 request.addEventListener("load", () => {
   const data = JSON.parse(request.responseText);
   console.log(data);
+});*/
+el = document.querySelector("img");
+el.src = "image2.jpg";
+
+btn.addEventListener("load", (e) => {
+  el.classList.add("fadeIn");
 });
